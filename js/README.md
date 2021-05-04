a70d533d4944ae1561b73fab78e4adbb231e9b15
ThibaudP <t.poncin@gmail.com>
2021-05-04 23:56:33 +0000

    feat: solution in js

---
+++ js/README.md
@@ -0,0 +1,16 @@
+# Simple Mastermind (Node.js version)
+Simple player vs computer Mastermind clone, in JavaScript
+
+## How to use
+Install Node.js
+```bash
+$ sudo apt install nodejs
+```
+Install readline-sync
+```bash
+$ ssudo npm install readline-sync
+```
+Run
+```bash
+$ ./mastermind.js
+```
---
+++ js/mastermind.js
@@ -0,0 +1,74 @@
+#!/usr/bin/node
+// Simple Mastermind clone in js
+
+const mastermind = function () {
+  const readline = require('readline-sync');
+  const level = 4;
+  const digits = 6;
+  const nbTurns = 12;
+  let turn = 1;
+  const secretCode = [];
+  let code, right, miss;
+
+  for (let i = 0; i < level; i++) {
+    const rand = String(Math.floor(Math.random() * digits) + 1);
+    secretCode.push(rand);
+  }
+
+  console.log('');
+  console.log('Welcome to Mastermind! (Node.js version)');
+  console.log('The code is 4 digits long, chosen among 6 possible digits (1 to 6)');
+  console.log('Can you find it in less than 12 turns?');
+  console.log('');
+  // console.log('DEBUG: ' + secretCode.join(''));
+
+  while (turn <= nbTurns) {
+    code = secretCode.slice();
+    right = 0;
+    miss = 0;
+    let play = readline.question(`Turn ${turn} - Your guess:\n`);
+    play = Array.from(play);
+
+    if (play.length !== 4) {
+      console.log(`\n(╯°□°)╯︵ ┻━┻\n\nYou lose!\nAnswer was ${secretCode.join('')}\n`);
+      return;
+    }
+
+    for (let i = 0; i < level; i++) {
+      if (code[i] === play[i]) {
+        right++;
+        code[i] = 'X';
+        play[i] = '#';
+      }
+    }
+
+    for (let i = 0; i < level; i++) {
+      for (let j = 0; j < level; j++) {
+        if (play[i] === code[j]) {
+          miss++;
+          play[i] = '*';
+        }
+      }
+    }
+
+    if (right === 4) {
+      console.log('\n(ง ͡ʘ ͜ʖ ͡ʘ)ง\n\nYou win! \\o/\n\n');
+    }
+
+    console.log(`Right: ${right} - Miss: ${miss}`);
+    /* Comment next block to disable hints */
+    for (let i = 0; i < level; i++) {
+      if (play[i] !== '#' && play[i] !== '*') {
+        play[i] = 'x';
+      }
+    }
+    console.log(`Hint: ${play.join('')} (#: Right, *: Miss, x: Wrong)\n`);
+
+    turn++;
+  }
+  console.log(`\n(╯°□°)╯︵ ┻━┻\n\nYou lose!\nAnswer was ${secretCode.join('')}\n`);
+};
+
+if (require.main === module) {
+  mastermind();
+}

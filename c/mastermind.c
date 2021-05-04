2a5f6ad0742587c1eebca0e253de6d1e033705a6
ThibaudP <t.poncin@gmail.com>
2021-05-04 19:37:08 +0000

    fix: broken miss logic

--- c/mastermind.c
+++ c/mastermind.c
@@ -14,7 +14,7 @@ int main(void)
 	int nb_turns = 12;
 	int turn = 1;
 	int random_num;
-	int i, j, right = 0, wrong = 0;
+	int i, j, right = 0, miss = 0;
 	char *code = malloc(sizeof(char) * 5);
 	char *secret_code = malloc(sizeof(char) * 5);
 	char *play = NULL;
@@ -27,20 +27,21 @@ int main(void)
 		secret_code[i] = random_num + '0';
 	}
 	secret_code[i] = '\0';
+	strcpy(secret_code, "1613");
 
 	printf("\n");
 	printf("Welcome to Mastermind! (C version)\n");
 	printf("The code is 4 digits long, chosen among 6 possible digits (1 to 6)\n");
 	printf("Can you find it in less than 12 turns?\n");
 	printf("\n");
-	/* printf("DEBUG: %s\n", secret_code); */
+	printf("DEBUG: %s\n", secret_code);
 	printf("\n");
 
 	while (turn <= nb_turns)
 	{
 		strcpy(code, secret_code);
 		right = 0;
-		wrong = 0;
+		miss = 0;
 
 		printf("Turn %d: Your guess:\n", turn);
 		getline(&play, &buf, stdin);
@@ -59,8 +60,8 @@ int main(void)
 			if (play[i] == code[i])
 			{
 				right += 1;
-				code[i] = '#';
-				play[i] = '*';
+				code[i] = 'X';
+				play[i] = '#';
 			}
 		}
 
@@ -68,10 +69,10 @@ int main(void)
 		{
 			for (j = 0; j < level; j++)
 			{
-				if (play[j] == code[i])
+				if (play[i] == code[j])
 				{
-					wrong += 1;
-					code[i] = '*';
+					miss += 1;
+					play[i] = '*';
 				}
 			}
 		}
@@ -85,7 +86,19 @@ int main(void)
 			return (0);
 		}
 
-		printf("Right: %d - Wrong: %d\n\n", right, wrong);
+		printf("Right: %d - Miss: %d\n", right, miss);
+		/* Comment next block to disable hints */
+		for (i = 0; i < level; i++)
+		{
+			if (play[i] != '#' && play[i] != '*')
+			{
+				play[i] = 'x';
+			}
+		}
+		printf("Hint: %s (#: Right, *: Miss, x: Wrong)\n", play);
+		
+		printf("\n");
+
 		turn += 1;
 	}
 

eac3553289f532578e272ed1ac9d3a6c2b1c6ddc
ThibaudP <t.poncin@gmail.com>
2021-05-06 00:08:54 +0000

    feat: add pascal version

---
+++ pascal/README.md
@@ -0,0 +1,19 @@
+# Simple Mastermind (Pascal version)
+Simple player vs computer Mastermind clone, in Pascal
+
+## How to use
+Install FreePascal
+```bash
+$ sudo apt install fpc
+```
+Compile
+```bash
+$ fpc mastermind.pas -o mastermind
+```
+Run
+```bash
+$ ./mastermind
+```
+
+## Utils
+VS Code extension: https://marketplace.visualstudio.com/items?itemName=alefragnani.pascal
---
+++ pascal/mastermind.pas
@@ -0,0 +1,98 @@
+program Mastermind;
+uses crt, sysutils, BaseUnix;
+
+var
+Levels : integer = 4;
+Digits : integer = 6;
+NbTurns : integer = 12;
+Turn : integer = 1;
+SecretCode : string = '';
+Code : string;
+Play : string;
+
+i, j : integer;
+Hit, Miss : integer;
+
+begin
+    randomize();
+    for i := 1 to Levels do SecretCode += IntToStr(Random(Digits) + 1);
+
+    Writeln;
+    Writeln('Welcome to Mastermind! (Pascal version)');
+    Writeln('The code is 4 digits long, chosen among 6 possible digits (1 to 6)');
+	Writeln('Can you find it in less than 12 turns?');
+    Writeln;
+    Writeln('DEBUG: ' + SecretCode);
+    Writeln;
+
+
+    while Turn <= NbTurns do
+    begin
+        Code := SecretCode;
+        Hit := 0;
+        Miss := 0;
+
+        Writeln('Turn ' + IntToStr(Turn) + ' - Your guess:');
+        Readln(Play);
+
+        if Length(Play) <> 4 then
+        begin
+            Writeln;
+            Writeln('"(╯°□°)╯︵ ┻━┻');
+            Writeln;
+            Writeln('You lose!');
+            Writeln('Answer was ' + SecretCode);
+            Writeln;
+            Halt(1);
+        end;
+
+        for i := 1 to Levels do
+        begin
+            if Play[i] = Code[i] then
+            begin
+                Hit += 1;
+                Code[i] := 'X';
+                Play[i] := '#';
+            end;
+        end;
+
+        for i := 1 to Levels do
+        begin
+            for j := 1 to Levels do
+            begin
+                if Play[i] = Code[j] then
+                begin
+                    Miss += 1;
+                    Play[i] := '*';
+                end;
+            end;
+        end;
+
+        if Hit = 4 then
+        begin
+            Writeln;
+            Writeln('(ง ͡ʘ ͜ʖ ͡ʘ)ง');
+            Writeln;
+            Writeln('You win! \o/');
+            Halt(1);
+        end;
+
+        Writeln('Right: ' + IntToStr(Hit) + ' - Miss: ' + IntToStr(Miss));
+
+        // Comment next block to disable hints
+        for i := 1 to Levels do
+            if (Play[i] <> '#') and (Play[i] <> '*') then Play[i] := 'x';
+        Writeln('Hint: ' + Play + ' (#: Right, *: Miss, x: Wrong)');
+        
+        Writeln;
+
+        Turn += 1;
+    end;
+
+    Writeln;
+    Writeln('"(╯°□°)╯︵ ┻━┻');
+    Writeln;
+    Writeln('You lose!');
+    Writeln('Answer was ' + SecretCode);
+    Writeln;
+end.
